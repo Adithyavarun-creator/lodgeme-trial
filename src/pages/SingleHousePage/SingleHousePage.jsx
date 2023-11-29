84fefbdf7fc2d529a4661eb42b34015d4c1b0652
Adithyavarun-creator <74570395+Adithyavarun-creator@users.noreply.github.com>
2023-11-29 23:22:29 +0530

    npm update

--- src/pages/SingleHousePage/SingleHousePage.jsx
+++ src/pages/SingleHousePage/SingleHousePage.jsx
@@ -293,9 +293,9 @@ const SingleHousePage = () => {
             </div>
           </div>
 
-          {/* <div>
+          <div className="" style={{ width: "100%" }}>
             <Mapbox />
-          </div> */}
+          </div>
         </SingleHousePageStyles>
       </HelmetProvider>
     </>
